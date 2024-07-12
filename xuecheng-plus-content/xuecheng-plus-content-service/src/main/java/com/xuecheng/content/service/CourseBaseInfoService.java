1d0f77f5b325b80219784921a31f888cca0ac881
daydream <daydream_wjh@qq.com>
2024-07-12 17:22:33 +0800

    课程查询mapper测试

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,22 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+/**
+ * @Author daydream
+ * @Description
+ * @Date 2024/7/12
+ */
+public interface CourseBaseInfoService {
+    /**
+     * 课程分页查询
+     *
+     * @param pageParams      分页查询参数
+     * @param courseParamsDto 查询条件
+     * @return 查询结果
+     */
+    public PageResult<CourseBase> queryCourseBase(PageParams pageParams, QueryCourseParamsDto courseParamsDto);
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseInfoServiceTests.java
@@ -0,0 +1,31 @@
+package com.xuecheng.content;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author daydream
+ * @Description
+ * @Date 2024/7/12
+ */
+@SpringBootTest
+public class CourseBaseInfoServiceTests {
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
+    @Test
+    public void testCourseBaseInfoService() {
+        QueryCourseParamsDto courseParamsDto = new QueryCourseParamsDto();
+        PageParams pageParams = new PageParams(6L, 1L);
+        courseParamsDto.setCourseName("java");
+        courseParamsDto.setAuditStatus("202004");
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBase(pageParams, courseParamsDto);
+        System.out.println(courseBasePageResult);
+    }
+}
