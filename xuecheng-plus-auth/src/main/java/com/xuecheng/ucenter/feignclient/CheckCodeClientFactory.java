e9ecfacc79f112698c8205146d19a993361d5d88
daydream <daydream_wjh@qq.com>
2024-09-14 11:48:53 +0800

    9月14日 用户认证

---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/feignclient/CheckCodeClientFactory.java
@@ -0,0 +1,25 @@
+package com.xuecheng.ucenter.feignclient;
+
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author daydream
+ * Description
+ * Date 2024/9/14 11:05
+ */
+@Slf4j
+@Component
+public class CheckCodeClientFactory implements FallbackFactory<CheckCodeClient> {
+    @Override
+    public CheckCodeClient create(Throwable throwable) {
+        return new CheckCodeClient() {
+            @Override
+            public Boolean verify(String key, String code) {
+                log.debug("调用验证码服务熔断异常:{}", throwable.getMessage());
+                return null;
+            }
+        };
+    }
+}
--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/UserServiceImpl.java
@@ -4,9 +4,12 @@ import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
+import com.xuecheng.ucenter.model.dto.XcUserExt;
 import com.xuecheng.ucenter.model.po.XcUser;
+import com.xuecheng.ucenter.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +26,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserDetailsService {
     @Autowired
     XcUserMapper xcUserMapper;
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
@@ -33,17 +38,19 @@ public class UserServiceImpl implements UserDetailsService {
             log.info("认证请求不符合项目要求:{}", s);
             throw new RuntimeException("认证请求数据格式不对");
         }
-        String username = authParamsDto.getUsername();
-        XcUser user = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>()
-                .eq(XcUser::getUsername, username));
-        if (user == null) {
-            //返回空表示用户不存在
-            return null;
-        }
+        String authType = authParamsDto.getAuthType();
+        AuthService authService = applicationContext.getBean(authType + "_authservice", AuthService.class);
+        XcUserExt user = authService.execute(authParamsDto);
+        return getUserPrincipal(user);
+    }
+
+    public UserDetails getUserPrincipal(XcUserExt user) {
         String password = user.getPassword();
         String[] authorities = {"p1"};
+        user.setPassword(null);
         String userString = JSON.toJSONString(user);
         UserDetails userDetails = User.withUsername(userString).password(password).authorities(authorities).build();
         return userDetails;
     }
+
 }
